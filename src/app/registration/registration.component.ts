a5ba3aa39b689a68f7f90d08df9a8b57e598cf0c
subodhsawan <subodh.sawan@gmail.com>
2021-01-13 22:41:56 +0530

    'sfdgg'

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
+import { StudentserviceService } from './../studentservice.service';
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public Students: Student=new Student();
+ public msg;
+ public  rex=/^[A-z]/;
+  constructor(private service: StudentserviceService) { }
 
   ngOnInit(): void {
+
   }
+
+
+  public savestudent(){
+
+     if (this.Students.id != null&&this.Students.id != undefined)
+
+      if  (this.Students.name != null  && this.Students.name != undefined && this.Students.name.match(this.rex))
+        if (this.Students.rollnumber != null&&this.Students.rollnumber != undefined)
+          if (this.Students.book != null&&this.Students.book != undefined)
+            if (this.Students.fees != null&&this.Students.fees != undefined){
+      this.service.savestudentinfo(this.Students).subscribe(
+
+        data=>{this.msg=data;
+          console.log(data);
+          this.Students=new Student()},
+        error=>{console.log(error);
+        }
+
+        );
+     }
+
+     else{
+       alert("no data");
+     }
+        }
 
 }
